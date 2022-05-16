52dd831afe9a9cb9e1a2e4aa411ba37fae1a53b9
cosmin <cosmincolceru@gmail.com>
2022-05-16 16:12:37 +0300

    modified navbar and added gallery

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ const port = 3000;
 
 const users = [];
 
+app.use(express.static(__dirname + '/views'));
 app.set('view-engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 app.use(flash())
---
+++ views/script.js
@@ -0,0 +1,38 @@
+let slideIndex = 1;
+
+
+
+function showSlides(n) {
+	
+	let slides = document.getElementsByClassName("slide");
+	let dots = document.getElementsByClassName("dot");
+	if (n > slides.length) {
+		slideIndex = 1;
+	}
+	if (n < 1) {
+		slideIndex = slides.length;
+	}
+	for (let i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	for (let i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(" active", "");
+	}
+	slides[slideIndex - 1].style.display = "block";
+	dots[slideIndex - 1].className += " active";	
+}
+
+function plusSlides(n) {
+	showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+	slideIndex = n;
+	showSlides(slideIndex);
+}
+
+window.onload = init;
+
+function init() {
+	showSlides(slideIndex);	
+}
